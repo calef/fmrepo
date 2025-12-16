f98673f4fff8247fb626e7cd1140bad75835ad8a
Chris Alef <chris@crickertech.com>
2025-12-16 14:27:07 -0800

    add code coverage

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem 'mutex_m'
 gem 'ostruct'
 gem 'rake', '~> 13.0'
 gem 'rubocop', '~> 1.66', require: false
+gem 'simplecov', '~> 0.22', require: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter '/test/'
+  add_group 'Library', 'lib'
+end
+
 require 'fmrepo'
 
 require 'minitest/autorun'
